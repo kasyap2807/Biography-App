d3fcd91a7b0e55d8755921afa60e3db67046885e
Krishna Kasyap <125851796+kasyap2807@users.noreply.github.com>
2023-07-28 21:16:24 +0530

    Add files via upload

---
+++ BIOGRAPHY/design.js
@@ -0,0 +1,23 @@
+ReactDOM.render(
+    <div>
+    <h1>John McCarthy</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/John_McCarthy_Stanford.jpg/330px-John_McCarthy_Stanford.jpg"></img>
+    <h3>Born	September 4, 1927
+Boston, Massachusetts, U.S.
+Died	October 24, 2011 (aged 84)
+Stanford, California, U.S.</h3>
+<h3>
+Awards	Turing Award (1971)
+Computer Pioneer Award (1985)
+IJCAI Award for Research Excellence (1985)
+Kyoto Prize (1988)
+National Medal of Science (1990)
+Benjamin Franklin Medal (2003)
+</h3>
+<p>
+John McCarthy (September 4, 1927 – October 24, 2011) was an American computer scientist and cognitive scientist. He was one of the founders of the discipline of artificial intelligence.[1] He co-authored the document that coined the term "artificial intelligence" (AI), developed the programming language family Lisp, significantly influenced the design of the language ALGOL, popularized time-sharing, and invented garbage collection.
+
+McCarthy spent most of his career at Stanford University.[2] He received many accolades and honors, such as the 1971 Turing Award for his contributions to the topic of AI,[3] the United States National Medal of Science, and the Kyoto Prize.
+</p>
+    </div>,
+    document.getElementById("content"));
